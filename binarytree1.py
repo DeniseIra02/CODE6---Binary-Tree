0b55d561b36c1958c2ba5afa62ab4723e12a429e
DeniseIra02 <irajubilo@gmail.com>
2023-01-14 11:53:25 +0800

    add an elements list and if conditions to visit left tree, base node and right tree.

--- binarytree1.py
+++ binarytree1.py
@@ -28,4 +28,18 @@ class BinarySearchTreeNode:
     
     #method that return the list of elements in binary tree in specific order       
     def in_order_traversal(self):
+        #add list 
+        elements = []
+        #visit left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        #visit base node
+        elements.append(self.data)
+
+        #visit right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
     
