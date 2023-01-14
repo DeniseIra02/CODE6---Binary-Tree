e5209db056036b46c753573d41dea01ccd797828
DeniseIra02 <irajubilo@gmail.com>
2023-01-14 13:06:36 +0800

    Program complete.

--- binarytree1.py
+++ binarytree1.py
@@ -85,3 +85,6 @@ if __name__ == '__main__':
     #alphabetical order
     print(country_tree.in_order_traversal())
     
+    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+    numbers_tree = build_tree(numbers)
+    print("sorted list:", numbers_tree.in_order_traversal())
