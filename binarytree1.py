33fc4c822933ba088af14487c3c3e69b2c42a39b
DeniseIra02 <irajubilo@gmail.com>
2023-01-14 11:23:13 +0800

    add a def function for the method of checking the value

--- binarytree1.py
+++ binarytree1.py
@@ -1,4 +1,4 @@
-#general tree which can have any number/letter of elements  
+#general tree which node can have any of the elements  
 class BinarySearchTreeNode:
     def __init__(self, data):
         self.data = data
@@ -6,3 +6,8 @@ class BinarySearchTreeNode:
         self.left = None #elements are less than the current node
         #rightsearchtree
         self.right = None #elements are greater than the current node
+     
+    #method of checking the value 
+    def add_child(self, data):
+        if data == self.data:
+            return
